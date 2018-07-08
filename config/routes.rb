df8bb4643073464489fd17863fd64c0b8702be02
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-07-08 10:09:41 +0200

    Setup correct routes for navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
+  resources :countries, only: [:index]
+  resources :trips, only: [:index]
+
   get 'countries/:country_name', to: 'countries#show', as: :country
 
 
